d0a5d717f17550510a78ef0dd94a6c1d1dbb4e48
Djangodev2010 <gaminglz507@gmail.com>
2026-06-30 10:42:14 +0530

    feat: created the Snippet model, SnippetSerializer, UserSerializer, SnippetList, SnippetDetail, UserList, UserDetail. Added permissions class to SnippetList and SnippetDetail to allow read only permissions for unauthenticated users. Added default rest_framework urls for browsable API authentication.

--- drf_practice/core_app/models.py
+++ drf_practice/core_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+class Snippet(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=155, blank=True, default="")
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, default="Python", max_length=100
+    )
+    style = models.CharField(
+        choices=STYLE_CHOICES, default="friendly", max_length=100
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="snippets")
+    highlighted = models.TextField()
+    
+    class Meta:
+        ordering = ['created_at']
+        
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        """
+        Using the pygments library to create a highlighted HTML representaion of
+        of the code snippet
+        """
+        
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
+        
+
+
+
---
+++ drf_practice/core_app/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Snippet
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.ModelSerializer):
+    """
+    Snippet serializer for serialization and deserialization of the instances
+    of our snippet model
+    """
+    
+    # Setting the owner field to ReadOnlyField to only use it for displaying the
+    # owner's username and is populated with the owner's username
+    owner = serializers.ReadOnlyField(source="owner.username")
+    
+    class Meta:
+        model = Snippet
+        fields = [
+            'id',
+            'title',
+            'code',
+            'language',
+            'linenos',
+            'style',
+            'created_at',
+            'owner'
+        ]
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    UserSerializer for serialization and deserialization of the instances
+    of the User model
+    """
+    
+    # The snippets field to display the pk of all the snippets associated with 
+    # the user
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all()
+    )
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
